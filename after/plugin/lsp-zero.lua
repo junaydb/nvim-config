d05419ffe16095ad983666b5da577a311cad2106
Junayd Bhoyroo <hello@junaydbhoyroo.com>
2023-06-30 10:44:32 +0100

    update lsp config

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,79 +1,65 @@
 local lsp = require("lsp-zero").preset({
-    manage_nvim_cmp = {
-        set_sources = "recommended",
-    },
+	manage_nvim_cmp = {
+		set_sources = "recommended",
+	},
 })
 
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
+	lsp.default_keymaps({ buffer = bufnr })
 
-    -- show diagnostics on hover
-    vim.api.nvim_create_autocmd("CursorHold", {
-        buffer = bufnr,
-        callback = function()
-            local opts = {
-                focusable = false,
-                close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-                border = "rounded",
-                source = "always",
-                prefix = " ",
-                scope = "cursor",
-            }
-            vim.diagnostic.open_float(nil, opts)
-        end,
-    })
-
-    -- show variable info
-    vim.keymap.set("n", "<leader>d", vim.lsp.buf.hover, {})
+	-- show diagnostics on hover
+	vim.api.nvim_create_autocmd("CursorHold", {
+		buffer = bufnr,
+		callback = function()
+			local opts = {
+				focusable = false,
+				close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+				border = "rounded",
+				source = "always",
+				prefix = " ",
+				scope = "cursor",
+			}
+			vim.diagnostic.open_float(nil, opts)
+		end,
+	})
 end)
+
+-- show variable info
+vim.keymap.set("n", "<leader>d", vim.lsp.buf.hover, {})
 
 -- no sign icons
 lsp.set_sign_icons({})
 
-lsp.format_on_save({
-    format_opts = {
-        async = false,
-        timeout_ms = 10000,
-    },
-    servers = {
-        ['lua_ls'] = { 'lua' },
-        ['null-ls'] = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'json' },
-    }
-})
-
 -- disable tsserver formatting
 require("lspconfig").tsserver.setup({
-    on_init = function(client)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentFormattingRangeProvider = false
-    end,
+	on_init = function(client)
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentFormattingRangeProvider = false
+	end,
 
-    -- for import module alias completion
-    init_options = {
-        preferences = {
-            importModuleSpecifierPreference = "non-relative",
-        },
-    },
+	-- for import module alias completion
+	init_options = {
+		preferences = {
+			importModuleSpecifierPreference = "non-relative",
+		},
+	},
 })
 
--- disable lua_ls formatting
 require("lspconfig").lua_ls.setup({
-    on_init = function(client)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentFormattingRangeProvider = false
-    end,
-})
+	-- disable lua_ls formatting
+	on_init = function(client)
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentFormattingRangeProvider = false
+	end,
 
--- set 'vim' as global for lua
-require("lspconfig").lua_ls.setup({
-    -- ... other configs
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-        },
-    },
+	-- set 'vim' as global for lua
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
 
 lsp.setup()
@@ -83,13 +69,13 @@ local cmp = require("cmp")
 local cmp_action = require("lsp-zero").cmp_action()
 
 cmp.setup({
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    mapping = {
-        ["<Tab>"] = cmp_action.luasnip_supertab(),
-        ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    },
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = {
+		["<Tab>"] = cmp_action.luasnip_supertab(),
+		["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	},
 })
