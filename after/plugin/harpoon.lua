db5ea38bae3d5b51120d1248162fd91e1a9a6027
Junayd Bhoyroo <hello@junaydbhoyroo.com>
2023-07-01 12:17:20 +0100

    update harpoon mappings

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,6 +1,9 @@
 local opts = { noremap = true }
+
 local harpoonMark = require("harpoon.mark").add_file
-local harpoonQuickMenu = require("harpoon.ui").toggle_quick_menu
+local harpoonUI = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", harpoonMark, opts)
-vim.keymap.set("n", "<leader>t", harpoonQuickMenu, opts)
+vim.keymap.set("n", "<leader>pj", harpoonUI.toggle_quick_menu, opts)
+vim.keymap.set("n", "<leader>j", harpoonUI.nav_next, opts)
+vim.keymap.set("n", "<leader>k", harpoonUI.nav_prev, opts)
