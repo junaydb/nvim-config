ce85221aa3a5177a8ecc334a904266e0e71d90c5
Junayd Bhoyroo <hello@junaydbhoyroo.com>
2023-07-02 03:48:23 +0100

    update comment mapping

--- after/plugin/nerdcommenter.lua
+++ after/plugin/nerdcommenter.lua
@@ -2,5 +2,4 @@ vim.g.NERDSpaceDelims = 1
 vim.g.NERDCreateDefaultMappings = 1
 
 local opts = { noremap = false }
-vim.keymap.set({"n", "v"}, "<leader>/", "<plug>NERDCommenterAlignLeft", opts)
-vim.keymap.set({"n", "v"}, "<leader>u", "<plug>NERDCommenterUncomment", opts)
+vim.keymap.set({"n", "v"}, "<C-/>", "<plug>NERDCommenterToggle", opts)
