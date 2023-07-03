1f0aa5a513a25dc5f07c3bce92417c4c12f719e8
Junayd Bhoyroo <hello@junaydbhoyroo.com>
2023-07-03 03:29:55 +0100

    add harpoon mapping and disable autopairs

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,9 +1,10 @@
 local opts = { noremap = true }
 
-local harpoonMark = require("harpoon.mark").add_file
+local harpoonMark = require("harpoon.mark")
 local harpoonUI = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", harpoonMark, opts)
+vim.keymap.set("n", "<leader>a", harpoonMark.add_file, opts)
+vim.keymap.set("n", "<leader>pc", harpoonMark.clear_all, opts)
 vim.keymap.set("n", "<leader>pj", harpoonUI.toggle_quick_menu, opts)
 vim.keymap.set("n", "<leader>j", harpoonUI.nav_next, opts)
 vim.keymap.set("n", "<leader>k", harpoonUI.nav_prev, opts)
